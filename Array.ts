a92d901c9d0f157743958cd75ad44eaf9ee60dde
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-11-26 21:24:30 +0100

    array

---
+++ Array.ts
@@ -0,0 +1,11 @@
+// Array
+
+let numbers: number[] = [1, 2, 3, 4];
+
+// lub
+
+let my_numbers: Array<number> = [1, 2, 3, 4];
+
+let user: [string, string, number];
+user = ["Admin", "All", 1];
+console.log(user[0]);
